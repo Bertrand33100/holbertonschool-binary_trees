2e4aa4835df1f646341cbd16fc792d88d93e3bda
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-08-03 12:47:26 +0200

    7. In-order traversal

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - Parcourt un arbre binaire en ordre.
+ * @tree: Pointeur vers la racine de l'arbre.
+ * @func: Fonction à appeler pour chaque nœud.
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
