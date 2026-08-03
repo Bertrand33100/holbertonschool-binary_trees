7dfd1ddba740d5c6fd888dc39922a3e7daa1ff56
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-08-03 15:29:24 +0200

    14. Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,36 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - Calcule le facteur d'équilibre d'un arbre binaire.
+ * height - Calcule la hauteur d'un arbre binaire.
+ * @tree: Pointeur vers la racine de l'arbre.
+ *
+ * Return: La hauteur de l'arbre.
+ */
+static size_t height(const binary_tree_t *tree)
+{
+	size_t left;
+	size_t right;
+
+	if (tree == NULL)
+		return (0);
+
+	left = 0;
+	right = 0;
+
+	if (tree->left != NULL)
+		left = 1 + height(tree->left);
+
+	if (tree->right != NULL)
+		right = 1 + height(tree->right);
+
+	if (left > right)
+		return (left);
+
+	return (right);
+}
+
+/**
+ * binary_tree_balance - Mesure le facteur d'équilibre d'un arbre binaire.
  * @tree: Pointeur vers la racine de l'arbre.
  *
  * Return: Le facteur d'équilibre, ou 0 si tree est NULL.
@@ -18,10 +47,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 	right_height = 0;
 
 	if (tree->left != NULL)
-		left_height = binary_tree_height(tree->left) + 1;
+		left_height = height(tree->left) + 1;
 
 	if (tree->right != NULL)
-		right_height = binary_tree_height(tree->right) + 1;
+		right_height = height(tree->right) + 1;
 
 	return (left_height - right_height);
 }
