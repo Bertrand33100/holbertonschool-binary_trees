0a3b551dbb45c1cb70cd5af5b6dcca9801160a2b
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-08-03 21:42:24 +0200

    18. Uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Retourne l'oncle d'un nœud.
+ * @node: Pointeur vers le nœud.
+ *
+ * Return: Pointeur vers l'oncle du nœud, ou NULL si aucun.
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *grand_parent;
+
+	if (node == NULL || node->parent == NULL ||
+		node->parent->parent == NULL)
+		return (NULL);
+
+	grand_parent = node->parent->parent;
+
+	if (grand_parent->left == node->parent)
+		return (grand_parent->right);
+
+	return (grand_parent->left);
+}
